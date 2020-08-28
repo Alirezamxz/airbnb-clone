3ff3e78318761a5356d949b4ba4a354c262106c2
Alirezamxz <alirezamz.org@Gmail.com>
2020-08-28 13:50:27 +0430

    search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+import { DateRangePicker } from 'react-date-range'
+import PeopleIcon from '@material-ui/icons/People'
+import { Button } from '@material-ui/core'
 
 function Search() {
+
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection",
+    }
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+
     return (
         <div className="search">
-            <h2></h2>
+            <DateRangePicker  ranges={[selectionRange]} onChange={handleSelect}/>
+            <h2>Number of guests <PeopleIcon /></h2>
+            <input min={1} defaultValue={1} type="number" />
+            <Button>Search Airbnb</Button>
         </div>
     )
 }
