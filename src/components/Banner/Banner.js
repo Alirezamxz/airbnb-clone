12ddc02d3efcf52630b81367a00d40978bdbe38c
Alirezamxz <alirezamz.org@Gmail.com>
2020-08-28 12:59:35 +0430

    components

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import './Banner.css'
+import { Button } from "@material-ui/core"
+import Search from '../Search/Search'
+
+function Banner() {
+
+    const [showSearch, setShowSearch] = useState(false)
+
+    return (
+        <div className="banner">
+            <div className="banner__search">
+                {showSearch && <Search />}
+                <Button  onClick={() => setShowSearch(!showSearch)} variant="outlined" className="banner__searchButton">Search Dates</Button>
+            </div>
+            <div className="banner__info">
+                <h1>Get out and stretch your imagination</h1>
+                <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
+                <Button variant="outlined">Explore nearby</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Home.css'
+import Banner from '../Banner/Banner'
 
 
 function Home() {
     return (
         <div className="home">
-            <h1>Home component</h1>
+            
+        <Banner />
+
         </div>
     )
 }
