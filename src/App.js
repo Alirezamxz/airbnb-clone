625d428bb529564e759bcb4073eaca82cd78794a
Alirezamxz <alirezamz.org@Gmail.com>
2020-08-28 11:27:18 +0430

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>ok</h1>
+
+      <Header />
+
+      <Home />
+
     </div>
   );
 }
