1adf3da904cfbd272c4180ab0e3cbf9eb95a40c0
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2020-10-01 08:59:47 +0330

    Formating

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React from 'react'
-import './App.css'
-import Header from './components/Header/Header'
-import Home from './components/Home/Home'
-import SearchPage from './components/SearchPage/SearchPage'
-import Footer from './components/Footer/Footer'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import SearchPage from "./components/SearchPage/SearchPage";
+import Footer from "./components/Footer/Footer";
 // router
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Router>
-
         <Header />
 
-          <Switch>
-
-          <Route path="/search">  
+        <Switch>
+          <Route path="/search">
             <SearchPage />
           </Route>
 
           <Route path="/">
             <Home />
           </Route>
-            
-          </Switch>
-        
-        <Footer />
+        </Switch>
 
+        <Footer />
       </Router>
     </div>
   );
